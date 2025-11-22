c98d2b6f6936203d38cd44f925d025fec9460d23
ranikommineni <rani.kommineni@slu.edu>
2025-11-22 19:58:17 +0000

    DONE

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ app.get('/products/:id', api.getProduct)
 app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
+
+app.post('/orders', api.createOrder)
+app.get('/orders/:id', api.editOrder)
+app.get('/orders/:id', api.deleteOrder)
+app.post('/orders', api.listOrders)
+
+
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
-
